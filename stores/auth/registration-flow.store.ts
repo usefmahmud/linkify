3e78adf4e75fada385621003a09229ecbfce5fb2
usefmahmud <usef.mahmoud@outlook.com>
2025-08-01 17:36:28 +0300

    feat: add Zustand store for registration flow and user role management

---
+++ stores/auth/registration-flow.store.ts
@@ -0,0 +1,21 @@
+import { SignupSchema } from '@/schemas/auth/signup.schema';
+import { userRole } from '@/types/user';
+import { create } from 'zustand';
+
+interface RegistrationFlowStore {
+  userData: SignupSchema | null;
+  setUserData: (user: SignupSchema) => void;
+
+  userRole: userRole | null;
+  setUserRole: (role: userRole) => void;
+}
+
+export const useRegistrationFlowStore = create<RegistrationFlowStore>(
+  (set) => ({
+    userData: null,
+    setUserData: (user) => set({ userData: user }),
+
+    userRole: null,
+    setUserRole: (role) => set({ userRole: role }),
+  })
+);
